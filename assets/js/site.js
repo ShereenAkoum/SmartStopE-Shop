3216bda6acf5f842927dd5c81a3d62c812369258
ShereenAkoum <shereenhakoum@gmail.com>
2024-02-21 23:01:28 +0200

    1st commit

---
+++ assets/js/site.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  getCategories();
+});
+
+function getCategories() {
+  $.getJSON("./Json/categoryList.json", function (data) {
+    var categoryContainer = $("#categoryContainer");
+
+    var list = data.list;
+
+    list.forEach((category) => {
+      console.log(category.id);
+      console.log(category.name);
+      var categoryHTML = `
+        <div class="col-lg-3 col-md-6">
+          <a href="#" id="${category.id}">
+            <div class="item">
+              <div class="image">
+                <img
+                  src="${category.src}"
+                  alt="${category.name}"
+                  style="max-width: 44px"
+                />
+              </div>
+              <h4 class="category-name">${category.name}</h4>
+            </div>
+          </a>
+        </div>`;
+
+      // Append the HTML to categoryContainer
+      categoryContainer.append(categoryHTML);
+    });
+  }).fail(function () {
+    console.log("An error has occurred.");
+  });
+}
