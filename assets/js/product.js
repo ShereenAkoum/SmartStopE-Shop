e471ad7dff5c245651602b637bccee2af68f0971
ShereenAkoum <shereenhakoum@gmail.com>
2024-02-28 22:41:17 +0200

    check in

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,12 +1,9 @@
 var list = [];
 var fileTitle = "";
 
-
-
 $(document).ready(function () {
   var urlParams = new URLSearchParams(window.location.search);
   var categoryId = urlParams.get("categoryId");
-
 
   var url = "./Json/";
   if (categoryId == 1) {
@@ -29,208 +26,58 @@ $(document).ready(function () {
   }
 
   fetch(url + fileTitle + "CareProductList.json")
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
-    .then(data => {
+    .then((data) => {
       var productContainer = document.getElementById("productContainer");
       list = data.list;
 
       list.forEach(function (product) {
         if (product.active) {
-          var productHTML = '<div class="col-lg-2 col-6">' +
-            '<div class="item product-item" style="background-image: url(assets/images/products/' + fileTitle + 'Care/' + product.src + ');">' +
+          var productHTML =
+            '<div class="col-lg-2 col-6">' +
+            '<div class="item product-item" style="background-image: url(assets/images/products/' +
+            fileTitle +
+            "Care/" +
+            product.src +
+            ');">' +
             '<div class="thumb">' +
-            (product.discountedPrice !== 0 ?
-              '<span class="price price-discount"><em>' + product.currency + product.price + '</em>' + product.currency + product.discountedPrice + '</span>' :
-              '<span class="price">' + product.currency + product.price + '</span>') +
-            '</div>' +
+            (product.discountedPrice !== 0
+              ? '<span class="price price-discount"><em>' +
+                product.currency +
+                product.price +
+                "</em>" +
+                product.currency +
+                product.discountedPrice +
+                "</span>"
+              : '<span class="price">' +
+                product.currency +
+                product.price +
+                "</span>") +
+            "</div>" +
             '<div class="down-content">' +
-            '<a onclick="addToCart(\'' + product.SKU + '\')" data-sku="' + product.SKU + '" style="width:150px"><i class="fa fa-shopping-bag" style="margin-right:10px"></i>' + product.SKU + '</a>' +
-            '</div>' +
-            '</div>' +
-            '</div>';
-          productContainer.insertAdjacentHTML('beforeend', productHTML);
+            "<a onclick=\"addToCart('" +
+            product.SKU +
+            '\')" data-sku="' +
+            product.SKU +
+            '" style="width:150px"><i class="fa fa-shopping-bag" style="margin-right:10px"></i>' +
+            product.SKU +
+            "</a>" +
+            "</div>" +
+            "</div>" +
+            "</div>";
+          productContainer.insertAdjacentHTML("beforeend", productHTML);
         }
       });
     })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
     });
-
 });
-
-// Define the shopping cart object
-var shoppingCart = (function () {
-  var cart = [];
-
-  function Item(sku, name, price, count) {
-    this.sku = sku;
-    this.name = name;
-    this.price = price;
-    this.count = count;
-  }
-
-  function saveCart() {
-    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
-  }
-
-  function loadCart() {
-    cart = JSON.parse(sessionStorage.getItem('shoppingCart')) || [];
-  }
-
-  loadCart();
-
-  var obj = {};
-
-  obj.addItemToCart = function (sku, name, price, count) {
-    for (var item in cart) {
-      if (cart[item].sku == sku) {
-        cart[item].count ++;
-        saveCart();
-        return;
-      }
-    }
-    var item = new Item(sku, name, price, count);
-    cart.push(item);
-    saveCart();
-  };
-
-  obj.setCountForItem = function (sku, count) {
-    for (var i in cart) {
-      if (cart[i].sku == sku) {
-        cart[i].count = count;
-        break;
-      }
-    }
-    saveCart();
-  };
-
-  obj.removeItemFromCart = function (sku) {
-    for (var item in cart) {
-      if (cart[item].sku == sku) {
-        cart[item].count--;
-        if (cart[item].count === 0) {
-          cart.splice(item, 1);
-        }
-        break;
-      }
-    }
-    saveCart();
-  };
-
-  obj.removeItemFromCartAll = function (sku) {
-    for (var item in cart) {
-      if (cart[item].sku == sku) {
-        cart.splice(item, 1);
-        break;
-      }
-    }
-    saveCart();
-  };
-
-  obj.clearCart = function () {
-    cart = [];
-    saveCart();
-  };
-
-  obj.totalCount = function () {
-    var totalCount = 0;
-    for (var item in cart) {
-      totalCount += cart[item].count;
-    }
-    return totalCount;
-  };
-
-  obj.totalCart = function () {
-    var totalCart = 0;
-    for (var item in cart) {
-      totalCart += cart[item].price * cart[item].count;
-    }
-    return Number(totalCart.toFixed(2));
-  };
-
-  obj.listCart = function () {
-    var cartCopy = [];
-    for (var i in cart) {
-      item = cart[i];
-      itemCopy = {};
-      for (var p in item) {
-        itemCopy[p] = item[p];
-      }
-      itemCopy.total = Number(item.price * item.count).toFixed(2);
-      cartCopy.push(itemCopy);
-    }
-    return cartCopy;
-  };
-
-  return obj;
-})();
-
-// Update the addToCart function to use shoppingCart object
-function addToCart(SKU) {
-  var selectedProduct = list.find(product => product.SKU === SKU);
-  shoppingCart.addItemToCart(selectedProduct.SKU, selectedProduct.Name, selectedProduct.price, 1);
-  displayCart();
-}
-
-// Update the displayCart function to display shopping cart contents
-function displayCart() {
-  var cartArray = shoppingCart.listCart();
-  var output = "";
-  for (var i in cartArray) {
-    output += "<tr>"
-      + "<td>" + cartArray[i].name + "</td>"
-      + "<td>(" + cartArray[i].price + ")</td>"
-      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-sku='" + cartArray[i].sku + "' data-name=" + cartArray[i].name + ">-</button>"
-      + "<input type='number' class='item-count form-control' data-sku='" + cartArray[i].sku + "' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-      + "<button class='plus-item btn btn-primary input-group-addon' data-sku='" + cartArray[i].sku + "' data-name=" + cartArray[i].name + ">+</button></div></td>"
-      + "<td><button class='delete-item btn btn-danger' data-sku='" + cartArray[i].sku + "' data-name=" + cartArray[i].name + ">X</button></td>"
-      + " = "
-      + "<td>" + cartArray[i].total + "</td>"
-      + "</tr>";
-  }
-  $('.show-cart').html(output);
-  $('.total-cart').html(shoppingCart.totalCart());
-  $('.total-count').html(shoppingCart.totalCount());
-}
-
-// Clear items
-$('.clear-cart').click(function () {
-  shoppingCart.clearCart();
-  displayCart();
-});
-
-$('.show-cart').on("click", ".delete-item", function (event) {
-  var sku = $(this).data('sku');
-  shoppingCart.removeItemFromCartAll(sku);
-  displayCart();
-});
-
-$('.show-cart').on("click", ".minus-item", function (event) {
-  var sku = $(this).data('sku');
-  shoppingCart.removeItemFromCart(sku);
-  displayCart();
-});
-
-$('.show-cart').on("click", ".plus-item", function (event) {
-  var sku = $(this).data('sku');
-  shoppingCart.addItemToCart(sku);
-  displayCart();
-});
-
-$('.show-cart').on("change", ".item-count", function (event) {
-  var sku = $(this).data('sku');
-  var count = Number($(this).val());
-  shoppingCart.setCountForItem(sku, count);
-  displayCart();
-});
-
-// Initial display of cart
-displayCart();
 
 function openPopup(SKU) {
   var popupContainer = document.getElementById("popupContainer");
@@ -240,7 +87,7 @@ function openPopup(SKU) {
   for (const product of list) {
     if (product.SKU == SKU) {
       var Description = `
-      <table>
+      <table class='cartTable'>
         <tr>
           <td class="grey-bg">SKU</td>
           <td>${product.SKU}</td>
@@ -251,37 +98,41 @@ function openPopup(SKU) {
         </tr>
         <tr>
           <td class="grey-bg">Price</td>
-          ${product.discountedPrice !== 0
-          ? `<td style="color: red;"><span style="text-decoration: line-through;">${product.currency}${product.price}</span>&nbsp;<span>${product.currency}${product.discountedPrice}</span></td>`
-          : `<td class="price">${product.currency}${product.price}</td>`
-        }
+          ${
+            product.discountedPrice !== 0
+              ? `<td style="color: red;"><span style="text-decoration: line-through;">${product.currency}${product.price}</span>&nbsp;<span>${product.currency}${product.discountedPrice}</span></td>`
+              : `<td class="price">${product.currency}${product.price}</td>`
+          }
         </tr>
         <tr>
         <td class="grey-bg">Pack of 3</td>
-        ${product.packOf3Price !== 0
-          ? `<td>
+        ${
+          product.packOf3Price !== 0
+            ? `<td>
               ${product.currency}${product.packOf3Price}
             </td>`
-          : `<td>-</td>`
+            : `<td>-</td>`
         }
       </tr>
       <tr>
       <td class="grey-bg">Pack of 6</td>
-      ${product.packOf6Price !== 0
+      ${
+        product.packOf6Price !== 0
           ? `<td>
             ${product.currency}${product.packOf6Price}
           </td>`
           : `<td>-</td>`
-        }
+      }
     </tr>
     <tr>
     <td class="grey-bg">Pack of 12</td>
-    ${product.packOf12Price !== 0
-          ? `<td>
+    ${
+      product.packOf12Price !== 0
+        ? `<td>
           ${product.currency}${product.packOf12Price}
         </td>`
-          : `<td>-</td>`
-        }
+        : `<td>-</td>`
+    }
   </tr>
       </table>
     `;
