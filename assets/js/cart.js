b14429ad19446ffe9c5aa37ecefd9290d10fe542
Akoum-Shereen <shereen.akoum@inmobiles.net>
2024-02-29 15:59:09 +0200

    check in

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -141,7 +141,7 @@ function displayCart() {
       "</td>" +
       "<td><div class='input-group'>" +
       minusButton +
-      "<input type='tel' min = 1 class='item-count form-control' data-sku='" +
+      "<input type='tel' style='width:50px' min = 1 class='item-count form-control' data-sku='" +
       cartArray[i].sku +
       "' data-name='" +
       cartArray[i].name +
